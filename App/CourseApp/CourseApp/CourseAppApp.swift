fc8d0f504057663fa35fd193c01b97ae0718a801
Marcel Mravec <marcel.mravec@gmail.com>
2024-04-25 19:40:31 +0200

    Create folders create empty app

---
+++ App/CourseApp/CourseApp/CourseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseAppApp.swift
+//  CourseApp
+//
+//  Created by Marcel Mravec on 25.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct CourseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
