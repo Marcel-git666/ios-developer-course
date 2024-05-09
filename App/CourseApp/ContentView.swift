4cfb80a0056a01bf9a5dc1c04ce12503cb4450ab
Marcel Mravec <marcel.mravec@gmail.com>
2024-05-09 15:12:53 +0200

    add logger

--- App/CourseApp/ContentView.swift
+++ App/CourseApp/ContentView.swift
@@ -5,9 +5,11 @@
 //  Created by Marcel Mravec on 25.04.2024.
 //
 
+import os
 import SwiftUI
 
 struct ContentView: View {
+    var logger = Logger()
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -17,6 +19,9 @@ struct ContentView: View {
             Text("test")
         }
         .padding()
+        .onAppear {
+            logger.log("🦈 ContentView has appeared.")
+        }
     }
 }
 
