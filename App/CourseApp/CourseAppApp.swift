af6f0b0373e48f191b53b100c91f6d120f799876
Marcel Mravec <marcel.mravec@gmail.com>
2024-05-09 15:07:35 +0200

    add proper configuration add right plist for development for Firebase

--- App/CourseApp/CourseAppApp.swift
+++ App/CourseApp/CourseAppApp.swift
@@ -5,10 +5,22 @@
 //  Created by Marcel Mravec on 25.04.2024.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct CourseAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
