3d9027170219c8028a44a2518758adc45ee7de05
Marcel Mravec <marcel.mravec@gmail.com>
2024-05-13 08:47:55 +0200

    add labelCollectionViewCell add MockDataProvider

---
+++ App/CourseApp/Views/UIKit/UICollectionViewCells/LabelCollectionViewCell.swift
@@ -0,0 +1,52 @@
+//
+//  LabelCollectionViewCell.swift
+//  CourseApp
+//
+//  Created by Marcel Mravec on 12.05.2024.
+//
+
+import UIKit
+
+final class LabelCollectionViewCell: UICollectionViewCell {
+    
+    lazy var nameLabel = UILabel()
+    
+    // MARK: Lifecycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - Setup UI
+private extension LabelCollectionViewCell {
+    func setupUI() {
+        addSubviews()
+        configureLabel()
+        setupConstraints()
+    }
+    
+    func addSubviews() {
+        addSubview(nameLabel)
+    }
+    
+    func configureLabel() {
+        translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.textColor = .white
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
+            nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -5),
+            nameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            nameLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5)
+            
+        ])
+    }
+}
