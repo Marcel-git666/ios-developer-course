966c9031cde64223f948840d2da0abda2e5d7a53
Marcel Mravec <marcel.mravec@gmail.com>
2024-06-02 17:48:41 +0200

    add KeychainAcess 3rd party and make KeychainManager

---
+++ App/CourseApp/Common/Managers/KeychainManager.swift
@@ -0,0 +1,41 @@
+//
+//  KeychainManager.swift
+//  CourseApp
+//
+//  Created by Marcel Mravec on 02.06.2024.
+//
+
+import Foundation
+import KeychainAccess
+
+class KeychainManager: KeychainManaging {
+    private let keychain = Keychain()
+    private let encoder = JSONEncoder()
+    private let decoder = JSONDecoder()
+    
+    func store<T: Encodable>(key: String, value: T) throws {
+        do {
+            let data = try encoder.encode(value)
+            keychain[data: key] = data
+        } catch {
+            throw KeychainManagerError.encodingError(error)
+        }
+    }
+    
+    func fetch<T: Decodable>(key: String) throws -> T? { // swiftlint:disable:next conditional_returns_on_newline
+        guard let data = try keychain.getData(key) else { return nil }
+        do {
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            throw KeychainManagerError.decodingError(error)
+        }
+    }
+    
+    func remove(key: String) throws {
+        do {
+            try keychain.remove(key)
+        } catch {
+            throw KeychainManagerError.removeFailure(error)
+        }
+    }
+}
---
+++ App/CourseApp/Common/Managers/KeychainManaging.swift
@@ -0,0 +1,14 @@
+//
+//  KeychainManaging.swift
+//  CourseApp
+//
+//  Created by Marcel Mravec on 02.06.2024.
+//
+
+import Foundation
+
+protocol KeychainManaging {
+    func store<T: Encodable>(key: String, value: T) throws
+    func fetch<T: Decodable>(key: String) throws -> T?
+    func remove(key: String) throws
+}
